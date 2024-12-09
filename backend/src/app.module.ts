62aeea8fef46b30a21269afa8b35516c248a2ba3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-12-10 03:48:21 +0900

    dockerの設定

---
+++ backend/src/app.module.ts
@@ -0,0 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    //forRootはアプリ全体に適用するためのメソッド
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver, //driverサーバーを使用
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), //コードファーストで進める際にnest.jsから自動生成されるスキーマファイルの指定
+    }),
+  ],
+})
+export class AppModule {}
